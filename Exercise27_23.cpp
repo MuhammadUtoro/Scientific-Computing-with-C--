7a066660041863ea11194f1642126ded793a9fe1
Muhammad Utoro <dioutoro@googlemail.com>
2023-08-15 11:37:07 +0200

    Flow control Exercise part-III

---
+++ Exercise27_23.cpp
@@ -0,0 +1,30 @@
+/*
+Taken from the book: Guide to Scientific Computing in C++
+Exercise 2.7 sub 2.2
+*/
+
+#include <iostream>
+
+int main(){
+
+    int number;
+    int sum = 0;
+
+    while(sum <= 100){
+        std::cout << "Enter any integer value: ";
+        std::cin >> number;
+        sum += number;
+        std::cout << sum << std::endl;
+        if(number == -2){
+            std::cout << "Count will be reset!" << std::endl;
+            sum = 0;
+        }
+        if(sum >= 100){
+            std::cout << "The total sum of the integers = " << sum << std::endl;
+            std::cout << "Program is terminated!" << std::endl; 
+        }
+    }
+
+    return 0;
+
+}
