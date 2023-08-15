0a9e56cc6a013ba852b45daa88a41bb9d6b6a7f1
Muhammad Utoro <dioutoro@googlemail.com>
2023-08-15 11:35:26 +0200

    Flow control exercise

---
+++ Exercise27_21.cpp
@@ -0,0 +1,22 @@
+/*
+Taken from the book: Guide to Scientific Computing in C++
+Exercise 2.7 sub 2.1
+*/
+
+#include <iostream>
+
+int main(){
+    double x, y, z;
+    std::cout << "Enter x: ";
+    std::cin >> x;
+    std::cout << "Enter y: ";
+    std::cin >> y;
+
+    if((x >= y) || (x<5.0)){
+        z = 4.0;
+    }else{
+        z = 2.0;
+    }
+
+    std::cout << z << std::endl;
+}
