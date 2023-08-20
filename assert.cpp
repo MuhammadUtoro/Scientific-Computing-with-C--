76e500a13c298ba93b7714a51a5b0f8518ae15b9
Muhammad Utoro <dioutoro@googlemail.com>
2023-08-20 20:06:44 +0200

    Choose between NaN or error message

--- assert.cpp
+++ assert.cpp
@@ -7,7 +7,7 @@ int main(){
     float number;
     std::cout << "Enter non-negative number: " << std::endl;
     std::cin >> number;
-    assert(std::isfinite(number)); // resulting in NaN
+    // assert(std::isfinite(number)); // resulting in NaN
     assert(number >= 0.0); // resulting in Error message
     std::cout << "The square root of " << number;
     std::cout << " is: " << sqrt(number) << std::endl;
